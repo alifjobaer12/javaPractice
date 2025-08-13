cadd78358424fd59c8bc200a1340f7c4325c3b5e
alifjobaer12 <alifjobaer12@gmail.com>
2025-08-14 02:19:07 +0600

    14/08/25

---
+++ LabPractice/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+        public class Main {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                String s = sc.nextLine();
+                sc.close();
+                System.out.println(s);
+            }
+
+        }
